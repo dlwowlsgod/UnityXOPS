f847fba3a78ae8c5a5265fa454354d2cb4b8feee
dlwowlsgod <dlwowlsgod@gmail.com>
2025-03-03 11:16:20 +0900

    0.0.1

--- Assets/UnityXOPS/Scripts/Parameter/Character/CharacterRoleType.cs
+++ Assets/UnityXOPS/Scripts/Parameter/Character/CharacterRoleType.cs
@@ -3,9 +3,28 @@
 /// </summary>
 public enum CharacterRoleType
 {
+    /// <summary>
+    /// 시민 역할 유형입니다.
+    /// </summary>
     Civilian,
+
+    /// <summary>
+    /// 갱스터 역할 유형입니다.
+    /// </summary>
     Gangster,
+
+    /// <summary>
+    /// 테러리스트 역할 유형입니다.
+    /// </summary>
     Terrorist,
+
+    /// <summary>
+    /// 경찰 역할 유형입니다.
+    /// </summary>
     Cop,
+
+    /// <summary>
+    /// 군인 역할 유형입니다.
+    /// </summary>
     Soldier
 }
